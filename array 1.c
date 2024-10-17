d9f6ed2b1160b261d2e7f5c291ee97c99028115b
root <root@localhost>
2024-10-17 20:04:43 +0300

    auto publish 1729184682819

---
+++ array 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+ 
+    int marks[6]={40,43,48,56,67,70};
+    printf("%d\n",marks[0]);
+   printf("%d\n",marks[1]);
+   printf("%d\n",marks[2]);
+   printf("%d\n",marks[3]);
+   printf("%d\n",marks[4]);
+   printf("%d\n",marks[5]);
+    
+    return 0;
+}
